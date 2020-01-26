b65008b747ab428a978de63721c69848750ad388
T0YO3 <github.t0yo3@gmail.com>
2020-01-26 18:10:55 +0900

    Modify timestamp. Default is 2020/01/01. The camera image of 2020/01/05 is 2020/01/05.

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
     "flag"
     "log"
     "net/http"
+    "os"
+    "path/filepath"
+    "time"
 )
 
 var (
@@ -14,6 +17,19 @@ func main() {
     // オプション取得
     flag.IntVar(&day, "d", 0, "select day from 1 to 5. 0 (default) shows lists with multiple pages.")
     flag.Parse()
+
+    // 最終アクセス日時、更新日時を修正
+    loc, _ := time.LoadLocation("Local")
+    datetime := time.Date(2020, 1, 1, 0, 0, 0, 0, loc)
+    changeFileUpdatedAt(filepath.Join("static", "images"), datetime)
+    changeFileUpdatedAt(filepath.Join("static", "old", "20200101", "images"), datetime)
+    changeFileUpdatedAt(filepath.Join("static", "old", "20200102", "images"), datetime)
+    changeFileUpdatedAt(filepath.Join("static", "old", "20200103", "images"), datetime)
+    changeFileUpdatedAt(filepath.Join("static", "old", "20200104", "images"), datetime)
+    changeFileUpdatedAt(filepath.Join("static", "old", "20200105", "images"), datetime)
+    // 2020/01/05のカメラ画像の最終アクセス日時、更新日時を修正
+    datetime = time.Date(2020, 1, 5, 0, 0, 0, 0, loc)
+    os.Chtimes(filepath.Join("static", "old", "20200105", "images", "camera.jpg"), datetime, datetime)
 
     switch day {
     case 0:
@@ -42,4 +58,15 @@ func main() {
     log.Println("Listening...")
     // 3000ポートでサーバーを立ち上げる
     http.ListenAndServe(":3000", nil)
+}
+
+// 指定したフォルダ配下の最終アクセス日時、更新日時を修正する
+//
+// dirPath フォルダのパス
+// time    更新後の日時
+func changeFileUpdatedAt(dirPath string, time time.Time) {
+    files, _ := filepath.Glob(filepath.Join(dirPath, "*"))
+    for _, f := range files {
+        os.Chtimes(f, time, time)
+    }
 }
