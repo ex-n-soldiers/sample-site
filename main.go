967936958bbc40a17b0fd4f251145e3238d76369
mongolyy <mongolyy@gmail.com>
2020-01-26 12:28:25 +0900

    最終更新日を更新するfuncを追加

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
     "log"
     "net/http"
+    "os"
+    "path/filepath"
+    "time"
 )
 
 func main() {
+    // 最終アクセス日時、更新日時を2020/1/1 00:00:00に修正する
+    loc, _ := time.LoadLocation("Local")
+    changeFileUpdatedAt(filepath.Join("static", "images"), time.Date(2020, 1, 1, 0, 0, 0, 0, loc))
+
     //ディレクトリを指定する
     fs := http.FileServer(http.Dir("static"))
     //ルーティング設定。"/"というアクセスがきたらstaticディレクトリのコンテンツを表示させる
@@ -14,4 +21,15 @@ func main() {
     log.Println("Listening...")
     // 3000ポートでサーバーを立ち上げる
     http.ListenAndServe(":3000", nil)
+}
+
+// 指定したフォルダ配下の最終アクセス日時、更新日時を修正する
+//
+// dirPath フォルダのパス
+// time    更新後の日時
+func changeFileUpdatedAt(dirPath string, time time.Time) {
+    files, _ := filepath.Glob(filepath.Join(dirPath, "*"))
+    for _, f := range files {
+        os.Chtimes(f, time, time)
+    }
 }
